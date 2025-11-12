26b1da3b41b5d74ac5cdeee3fda3ba4a159e0b96
forfi4 <liamipomi@gmail.com>
2025-11-12 13:13:37 +0200

    Update string_utils.py

--- string_utils.py
+++ string_utils.py
@@ -1,6 +1,43 @@
-def split_before_each_uppercases(formula):
-    pass # Replace the `pass` with your code
+def split_at_digit(formula: str) -> tuple[str, int]:
+    first_digit_index = -1
+    
+    for i, char in enumerate(formula):
+        if char.isdigit():
+            first_digit_index = i
+            break
+            
+    if first_digit_index == -1:
+        return formula, 1
+        
+    else:
+        prefix = formula[:first_digit_index]
+        
+        number_str = formula[first_digit_index:]
+        
+        number = int(number_str)
+        
+        return prefix, number
 
 
-def split_at_first_digit(formula):
-    pass # Replace the `pass` with your code
+def split_before_each_uppercase(formula: str) -> list[str]:
+    if not formula:
+        return []
+
+    result = []
+    current_element = ""
+
+    for char in formula:
+        if char.isupper():
+          
+            if current_element:
+                result.append(current_element)
+            
+            current_element = char
+        else:
+            current_element += char
+            
+    if current_element:
+        result.append(current_element)
+        
+    return result
+
